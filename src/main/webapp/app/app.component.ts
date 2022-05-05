dd8c3573e34e99dcd5381c4286815ac02cc01dd3
离线请留言 <347061329@qq.com>
2022-05-05 18:51:13 +0800

    elemenu el-menu 动态菜单json解析

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -16,4 +16,29 @@ import '@/shared/config/dayjs';
     'jhi-footer': JhiFooter,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public activeIndex = '/';
+  // 左侧导航是否为鼠标划入显示二级菜单
+  public isCollapse = false;
+
+  // 动态加载菜单
+  public homeMenu = [
+    { index: '/', title: '导航1', children: [] },
+    {
+      index: '/example/uiexample',
+      title: '导航2',
+      children: [
+        { index: '/xx', title: '导航2-1', children: [] },
+        { index: '/xx2', title: '导航2-2', children: [] },
+      ],
+    },
+  ];
+
+  // el-menu选中会传入链接信息 key === index
+  public handleSelect(key, keyPath): void {
+    console.log('点击了select');
+    console.log(key, keyPath);
+    // 跳转指定地址
+    this.$router.push(key);
+  }
+}
--- src/main/webapp/app/app.vue
+++ src/main/webapp/app/app.vue
@@ -5,9 +5,46 @@
       <jhi-navbar></jhi-navbar>
     </div>
     <div class="container-fluid">
-      <div class="card jh-card">
-        <router-view></router-view>
-      </div>
+      <el-row>
+        <!-- 左侧菜单区 -->
+        <el-col :span="4">
+          <div class="grid-content bg-purple">
+            <!-- 动态左侧树 -->
+            <el-menu
+              :default-active="activeIndex"
+              class="el-menu-vertical-demo"
+              @select="handleSelect"
+              style="margin-top: 20px"
+              :collapse="isCollapse"
+            >
+              <template v-for="(item, index) in homeMenu">
+                <el-submenu :index="item.index.toString()" v-if="item.children.length != 0" :key="index * 30">
+                  <template slot="title">
+                    <i :class="item.icons"></i>
+                    <span slot="title">{{ item.title }}</span>
+                  </template>
+                  <el-menu-item :index="items.index.toString()" v-for="(items, indexs) in item.children" :key="indexs">
+                    {{ items.title }}
+                  </el-menu-item>
+                </el-submenu>
+                <el-menu-item :index="item.index.toString()" v-else :key="index * 2">
+                  <i :class="item.icons"></i>
+                  <span slot="title">{{ item.title }}</span>
+                </el-menu-item>
+              </template>
+            </el-menu>
+          </div>
+        </el-col>
+
+        <!-- 右侧内容区 -->
+        <el-col :span="20"
+          ><div class="grid-content bg-purple-light">
+            <div class="card jh-card">
+              <router-view></router-view>
+            </div></div
+        ></el-col>
+      </el-row>
+
       <b-modal id="login-page" hide-footer lazy>
         <span data-cy="loginTitle" slot="modal-title" id="login-title" v-text="$t('login.title')">Sign in</span>
         <login-form></login-form>
