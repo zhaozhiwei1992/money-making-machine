c317667d26a574d0116262a68e04d6d45f2f7d38
zhaozhiwei <347061329@qq.com>
2022-06-02 21:09:55 +0800

    日志不要使用sout

--- src/main/java/com/example/listener/QuartJobListener.java
+++ src/main/java/com/example/listener/QuartJobListener.java
@@ -3,28 +3,42 @@ package com.example.listener;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.JobListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * @Title: QuartJobListener
+ * @Package com/example/listener/QuartJobListener.java
+ * @Description:
+ * 可以在这里做监控
+ * @author zhaozhiwei
+ * @date 2022/6/2 下午5:39
+ * @version V1.0
+ */
 public class QuartJobListener implements JobListener {
+
+    private static final Logger log = LoggerFactory.getLogger(QuartJobListener.class);
 
     public static final String LISTENER_NAME = "QuartSchedulerListener";
 
     @Override
     public String getName() {
-        return LISTENER_NAME; //must return a name
+        //must return a name
+        return LISTENER_NAME;
     }
 
     //任务被调度前
     @Override
     public void jobToBeExecuted(JobExecutionContext context) {
         String jobName = context.getJobDetail().getKey().toString();
-        System.out.println("jobToBeExecuted");
-        System.out.println("Job : " + jobName + " is going to start...");
+        log.info("jobToBeExecuted");
+        log.info("Job : {} is going to start...", jobName);
     }
 
     //任务调度被拒了
     @Override
     public void jobExecutionVetoed(JobExecutionContext context) {
-        System.out.println("jobExecutionVetoed");
+        log.info("jobExecutionVetoed");
         //可以做一些日志记录原因
 
     }
@@ -32,13 +46,13 @@ public class QuartJobListener implements JobListener {
     //任务被调度后
     @Override
     public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
-        System.out.println("jobWasExecuted");
+        log.info("jobWasExecuted");
 
         String jobName = context.getJobDetail().getKey().toString();
-        System.out.println("Job : " + jobName + " is finished...");
+        log.info("Job : {} is finished...", jobName);
 
         if (jobException != null && !jobException.getMessage().equals("")) {
-            System.out.println("Exception thrown by: " + jobName + " Exception: " + jobException.getMessage());
+            log.error("Exception thrown by: " + jobName + " Exception: ", jobException);
         }
     }
 }
