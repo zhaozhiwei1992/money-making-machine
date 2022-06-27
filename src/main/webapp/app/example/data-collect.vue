cde36379606303d19eade8b14a4c02e70bebeaa8
zhaozhiwei <347061329@qq.com>
2022-06-27 21:45:06 +0800

    采集表

--- src/main/webapp/app/example/data-collect.vue
+++ src/main/webapp/app/example/data-collect.vue
@@ -28,7 +28,6 @@ export default {
       tableData: [],
       dialogExcelImportVisible: false,
       excelImportUrl: baseApiUrl + '/import',
-      excelTemplateUrl: baseApiUrl + '/exportTemplate',
     };
   },
   components: {
