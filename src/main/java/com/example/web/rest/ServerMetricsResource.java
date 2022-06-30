0db6095814141a96df56bfb756bcc7203dd254c7
zhaozhiwei <347061329@qq.com>
2022-06-30 22:56:41 +0800

    获取服务器相关信息

---
+++ src/main/java/com/example/web/rest/ServerMetricsResource.java
@@ -0,0 +1,26 @@
+package com.example.web.rest;
+
+import com.example.domain.Server;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhaozhiwei
+ * @version V1.0
+ * @Title: ServerMetricsResource
+ * @Package com/example/web/rest/ServerMetricsResource.java
+ * @Description: 服务监控信息
+ * @date 2022/6/30 下午10:18
+ */
+@RestController
+public class ServerMetricsResource {
+
+    @GetMapping("")
+    @RequestMapping("/sys/server/metrics")
+    public Server metrics() throws Exception {
+        final Server server = new Server();
+        server.copyTo();
+        return server;
+    }
+}
